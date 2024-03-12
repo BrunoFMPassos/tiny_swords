a0b12a057e6200d2d6bb9ff346a3166b27db27d4
BrunoFMPassos <bruno.si.passos27@gmail.com>
2024-03-12 18:00:17 -0300

    Finalização do player

--- knight/knight.gd
+++ knight/knight.gd
@@ -1,15 +1,24 @@
 extends CharacterBody2D
 
+@onready var attack_area_collision: CollisionShape2D = get_node("AttackArea/Collision")
 @onready var animation: AnimationPlayer = get_node("Animation")
+@onready var aux_animation_player: AnimationPlayer = get_node("AuxAnimationPlayer")
 @onready var texture: Sprite2D = get_node("Texture")
 
 var can_attack: bool = true
+var can_die: bool = false
 
+@export var health: int = 10
+@export var damage: int = 1
 @export var move_speed: float = 256.0
 
 func _physics_process(_delta: float) -> void:
-	if can_attack == false:
+	if (
+		can_attack == false or 
+		can_die
+	):
 		return
+		
 	move()
 	animate()
 	attack_handler()
@@ -28,9 +37,11 @@ func get_direction() -> Vector2:
 func animate() -> void:
 	if velocity.x > 0:
 		texture.flip_h = false
+		attack_area_collision.position.x = 58
 		
 	if velocity.x < 0:
 		texture.flip_h = true
+		attack_area_collision.position.x = -58
 	
 	if can_attack== false:
 		return
@@ -48,5 +59,24 @@ func attack_handler() -> void:
 	pass
 
 
-func _on_animation_finished(_anim_name: String) -> void:
-	can_attack = true
+func _on_animation_finished(anim_name: String) -> void:
+	match anim_name:
+		"attack":
+			can_attack = true
+		"death":
+			get_tree().reload_current_scene()
+
+
+func on_attack_area_body_entered(body) -> void:
+	body.update_health(damage)
+	
+func update_health(value: int) -> void:
+	health -= value
+	if health <= 0:
+		can_die = true
+		animation.play("death")
+		attack_area_collision.set_deferred("disabled",true)
+		
+		return
+		
+	aux_animation_player.play("hit")
